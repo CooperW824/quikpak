83ab9262e262d6741632461f966ce1501bd3f7b1
Cooper Werner <cooperwerner824@gmail.com>
2023-06-16 12:12:36 -0400

    Backend Done

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {};
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "quikpak": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "quikpakAuth": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/graphql/queries.js
@@ -0,0 +1,121 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getList = /* GraphQL */ `
+  query GetList($id: ID!) {
+    getList(id: $id) {
+      id
+      name
+      description
+      items {
+        items {
+          content
+          checked
+          listID
+          owner
+          id
+          createdAt
+          updatedAt
+          __typename
+        }
+        nextToken
+        __typename
+      }
+      owner
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listLists = /* GraphQL */ `
+  query ListLists(
+    $filter: ModelListFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listLists(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        items {
+          nextToken
+          __typename
+        }
+        owner
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getListItem = /* GraphQL */ `
+  query GetListItem($id: ID!) {
+    getListItem(id: $id) {
+      content
+      checked
+      listID
+      owner
+      id
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listListItems = /* GraphQL */ `
+  query ListListItems(
+    $filter: ModelListItemFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listListItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        content
+        checked
+        listID
+        owner
+        id
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const listItemsByListID = /* GraphQL */ `
+  query ListItemsByListID(
+    $listID: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelListItemFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listItemsByListID(
+      listID: $listID
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        content
+        checked
+        listID
+        owner
+        id
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
